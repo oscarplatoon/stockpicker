0f46fd86750ee15a13c49c3f6dc254693f4c74a3
jakewhite5 <jakewhite5@yahoo.com>
2021-06-03 07:58:08 -0700

    python solution with  unittesting

--- python/stock_picker_spec.py
+++ python/stock_picker_spec.py
@@ -2,6 +2,25 @@
 
 from stock_picker import picker
 
+import unittest
+
+class StockPicker(unittest.TestCase):
+
+    def test_for_functionality1(self):
+        self.assertEqual(picker([17,3,6,9,15,8,6,1,10]), (1,4))
+
+    def test_for_functionality2(self):
+        self.assertEqual(picker([3,17,6,9,18,15,6,1,10]), (0,4))
+
+    def test_for_functionality3(self):
+        self.assertEqual(picker([1,17,6,9,8,15,6,3,19]), (0,8))
+    
+    def test_for_functionality4(self):
+        self.assertEqual(picker([19,17,6,9,8,15,6,3,1]), (2,5))
+    
+if __name__ == '__main__':
+    unittest.main()
+
 print(picker([17,3,6,9,15,8,6,1,10]) == [1,4])
 print(picker([3,17,6,9,18,15,6,1,10]) == [0,4])
 print(picker([1,17,6,9,8,15,6,3,19]) == [0,8])
