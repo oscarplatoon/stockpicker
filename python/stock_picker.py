71ceeee1f4f707565516689d3481e3a74aaa52cb
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-03 15:21:50 -0400

    Andrew and Musa_stock picker-python solution

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,9 @@
 def picker(prices):
-    pass
+    high = 0
+    for i,price in enumerate(prices):
+        for x in range(i+1,len(prices)):
+            if(prices[x]-price) > high:
+                high = prices[x]-price
+                answer = [i,x]
+    return(answer)
+
