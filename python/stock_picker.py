aa08da6f525256043601393556538505317e4c1a
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-03 14:01:32 -0500

    Shawn's stock picker solution

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,33 @@
+# reverse the order of PRICES
+# input is a list of PRICES, newest day's
+# prices are lower indices
+# The best day to buy must be before the
+# best day to sell
+# Starting at the end of the array, store that BUY_INDICE
+# store the SELL_INDICE and DIFF between the first newer element that is less than the first elem
+# loop through the array comparing PRICES[INDICE] to each
+# newer day. if that comparison is > DIFF, store that SELL_INDICE and DIFF
+# repeat until you reach index 1
+
+
 def picker(prices):
-    pass
+    buy_indice = len(prices) - 1
+    sell_indice = None
+    diff = -1
+    i = buy_indice - 1
+    j = buy_indice
+ 
+    while j > 0:
+        while i >= 0:
+            temp_diff = prices[j] - prices[i]
+            #print(temp_diff)
+            if (temp_diff > 0 and temp_diff > diff):
+                diff = temp_diff
+                sell_indice = i
+                buy_indice = j
+            i -= 1
+        j -= 1
+        i = j - 1
+
+    return [sell_indice, buy_indice]
+
--- python/stock_picker_spec.py
+++ python/stock_picker_spec.py
@@ -1,8 +1,32 @@
-# Rewrite this in Unit Test
-
+import unittest
 from stock_picker import picker
 
-print(picker([17,3,6,9,15,8,6,1,10]) == [1,4])
-print(picker([3,17,6,9,18,15,6,1,10]) == [0,4])
-print(picker([1,17,6,9,8,15,6,3,19]) == [0,8])
-print(picker([19,17,6,9,8,15,6,3,1]) == [2,5])
+class StockPickerTestCases(unittest.TestCase):
+
+    def test_case_1(self):
+        output = picker([17,3,6,9,15,8,6,1,10])
+        self.assertEqual(output, [1,4])
+
+    def test_case_2(self):
+        output = picker([3,17,6,9,18,15,6,1,10])
+        self.assertEqual(output, [0,4])
+
+    def test_case_3(self):
+        output = picker([1,17,6,9,8,15,6,3,19])
+        self.assertEqual(output, [0,8])
+       
+    def test_case_4(self):
+        output = picker([19,17,6,9,8,15,6,3,1])
+        self.assertEqual(output, [2,5])
+    
+    def test_case_5(self):
+        output = picker([3, 3, 3, 3])
+        self.assertEqual(output, [None, 3])
+
+# print(picker([17,3,6,9,15,8,6,1,10]) == [1,4])
+# print(picker([3,17,6,9,18,15,6,1,10]) == [0,4])
+# print(picker([1,17,6,9,8,15,6,3,19]) == [0,8])
+# print(picker([19,17,6,9,8,15,6,3,1]) == [2,5])
+
+if __name__ == '__main__':
+    unittest.main()
