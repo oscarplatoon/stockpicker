754d633272cb165ca56d51d065ca1024baa9b84f
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-03 22:00:44 -0400

    Working o^2 logic, added tests for edge cases

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,4 +1,31 @@
-exports.picker = function(prices) {
-  resultArr = []
-  return resultArr
+exports.picker = function (prices) {
+  /**
+   * Define a function which receives an array, evaluates it for greatest difference from left
+   * to right, and returns the paired indices which give the greatest difference.
+   * 
+   * There are two null cases, one if the argument array is in reverse order, the other if
+   * there is only one element in the array.
+   */
+
+  // First edge case, only one or less items in argument array:
+  resultArr = [];
+  if (prices.length <= 1) {
+    return resultArr
+  }
+  let maxDiff = -1;
+
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if(prices[i] < prices[j] && maxDiff < (prices[j] - prices[i])) {
+        maxDiff = prices[j] - prices[i]
+        //set our output of best indices to new max difference indices.
+        resultArr[0] = i
+        resultArr[1] = j
+      } 
+    } 
+  }
+  if (maxDiff == -1) {
+  return ("No profitable pick in range.")
+  } else 
+  return resultArr 
 }
--- js/stockPicker.test.js
+++ js/stockPicker.test.js
@@ -7,10 +7,20 @@ test('does it return an array?', () => {
 })
 
 test('does it correctly pick stocks?', () => {
-  expect(shallowEqualArrays(sp.picker([17, 3, 6, 9, 15, 8, 6, 1, 10]))).toBe([1, 4])
+
+  expect(shallowEqualArrays(sp.picker([17, 3, 6, 9, 15, 8, 6, 1, 10]), [1, 4])).toBe(true)
+
+  expect(shallowEqualArrays(sp.picker([3, 17, 6, 9, 18, 15, 6, 1, 10]), [0, 4])).toBe(true)
+
+  expect(shallowEqualArrays(sp.picker([1, 17, 6, 9, 8, 15, 6, 3, 19]), [0, 8])).toBe(true)
+
+  expect(shallowEqualArrays(sp.picker([19, 17, 6, 9, 8, 15, 6, 3, 1]), [2, 5])).toBe(true)
 })
 
-// console.log(shallowEqualArrays(sp.picker([17,3,6,9,15,8,6,1,10]), [1,4]))
-// console.log(shallowEqualArrays(sp.picker([3,17,6,9,18,15,6,1,10]), [0,4]))
-// console.log(shallowEqualArrays(sp.picker([1,17,6,9,8,15,6,3,19]), [0,8]))
-// console.log(shallowEqualArrays(sp.picker([19,17,6,9,8,15,6,3,1]), [2,5]))
+test('does it handle the reverse sorted argument?', () => {
+  expect(sp.picker([5,4,3,2,1])).toBe("No profitable pick in range.")
+})
+
+test('Does it handle a blank argument?', () => {
+  expect(sp.picker([])).toEqual([])
+})
